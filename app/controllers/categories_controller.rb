45daf88e957fff7a207e6fad34e232c240ea9681
DaisukeTakuma <daisuke.takuma123@gmail.com>
2019-08-25 16:33:14 +0900

    カテゴリーごとの投稿一覧ページネーション機能追加

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,14 +1,13 @@
 class CategoriesController < ApplicationController
   def index
     if @category_name = Post.find_by(category_name: params[:category_name])
-      @posts = Post.where(category_name: params[:category_name])
+      @q = Post.where(category_name: params[:category_name]).ransack(params[:q])
+      @posts = @q.result(distinct: true).page(params[:page])
     else
       redirect_to root_path, notice: "投稿がまだありません"
     end
   end
 end
 
-def show
-  @q = Post.find_by(category_name: params[:category_name]).ransack(params[:q])
-  @post = @q.result(distinct: true).page(params[:page])
-end
+#アプリケーションコントローラーで設定している、before_actionのset_searchをsessions_controllerと、categories_controllerでは外したい。
+#before_actionの変数名と、コントローラーで定義している変数名はぶつかってしまうのか？？おそらく、対応するviewの部分でしか機能しないと思う。
