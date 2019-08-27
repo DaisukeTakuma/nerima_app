11e89cd6dd315c71c5c39b905ccfa06babeff36e
DaisukeTakuma <daisuke.takuma123@gmail.com>
2019-08-28 03:49:10 +0900

    カテゴリで選択した以外のグレーアウト機能追加

---
+++ app/assets/javascripts/posts.js
@@ -0,0 +1,231 @@
+
+  function categoryflg0(checked){
+    if(checked == true){
+      document.getElementById("category1").disabled = true;
+      document.getElementById("category2").disabled = true;
+      document.getElementById("category3").disabled = true;
+      document.getElementById("category4").disabled = true;
+      document.getElementById("category5").disabled = true;
+      document.getElementById("category6").disabled = true;
+      document.getElementById("category7").disabled = true;
+      document.getElementById("category8").disabled = true;
+      document.getElementById("category9").disabled = true;
+    } else {
+      document.getElementById("category1").disabled = false;
+      document.getElementById("category2").disabled = false;
+      document.getElementById("category3").disabled = false;
+      document.getElementById("category4").disabled = false;
+      document.getElementById("category5").disabled = false;
+      document.getElementById("category6").disabled = false;
+      document.getElementById("category7").disabled = false;
+      document.getElementById("category8").disabled = false;
+      document.getElementById("category9").disabled = false;
+    }
+  }
+    function categoryflg1(checked){
+    if(checked == true){
+      document.getElementById("category0").disabled = true;
+      document.getElementById("category2").disabled = true;
+      document.getElementById("category3").disabled = true;
+      document.getElementById("category4").disabled = true;
+      document.getElementById("category5").disabled = true;
+      document.getElementById("category6").disabled = true;
+      document.getElementById("category7").disabled = true;
+      document.getElementById("category8").disabled = true;
+      document.getElementById("category9").disabled = true;
+    } else {
+      document.getElementById("category0").disabled = false;
+      document.getElementById("category2").disabled = false;
+      document.getElementById("category3").disabled = false;
+      document.getElementById("category4").disabled = false;
+      document.getElementById("category5").disabled = false;
+      document.getElementById("category6").disabled = false;
+      document.getElementById("category7").disabled = false;
+      document.getElementById("category8").disabled = false;
+      document.getElementById("category9").disabled = false;
+    }
+  }
+  function categoryflg2(checked){
+    if(checked == true){
+      document.getElementById("category0").disabled = true;
+      document.getElementById("category1").disabled = true;
+      document.getElementById("category3").disabled = true;
+      document.getElementById("category4").disabled = true;
+      document.getElementById("category5").disabled = true;
+      document.getElementById("category6").disabled = true;
+      document.getElementById("category7").disabled = true;
+      document.getElementById("category8").disabled = true;
+      document.getElementById("category9").disabled = true;
+    } else {
+      document.getElementById("category0").disabled = false;
+      document.getElementById("category1").disabled = false;
+      document.getElementById("category3").disabled = false;
+      document.getElementById("category4").disabled = false;
+      document.getElementById("category5").disabled = false;
+      document.getElementById("category6").disabled = false;
+      document.getElementById("category7").disabled = false;
+      document.getElementById("category8").disabled = false;
+      document.getElementById("category9").disabled = false;
+    }
+  }
+  function categoryflg3(checked){
+    if(checked == true){
+      document.getElementById("category0").disabled = true;
+      document.getElementById("category1").disabled = true;
+      document.getElementById("category2").disabled = true;
+      document.getElementById("category4").disabled = true;
+      document.getElementById("category5").disabled = true;
+      document.getElementById("category6").disabled = true;
+      document.getElementById("category7").disabled = true;
+      document.getElementById("category8").disabled = true;
+      document.getElementById("category9").disabled = true;
+    } else {
+      document.getElementById("category0").disabled = false;
+      document.getElementById("category1").disabled = false;
+      document.getElementById("category2").disabled = false;
+      document.getElementById("category4").disabled = false;
+      document.getElementById("category5").disabled = false;
+      document.getElementById("category6").disabled = false;
+      document.getElementById("category7").disabled = false;
+      document.getElementById("category8").disabled = false;
+      document.getElementById("category9").disabled = false;
+    }
+  }
+  function categoryflg4(checked){
+    if(checked == true){
+      document.getElementById("category0").disabled = true;
+      document.getElementById("category1").disabled = true;
+      document.getElementById("category2").disabled = true;
+      document.getElementById("category3").disabled = true;
+      document.getElementById("category5").disabled = true;
+      document.getElementById("category6").disabled = true;
+      document.getElementById("category7").disabled = true;
+      document.getElementById("category8").disabled = true;
+      document.getElementById("category9").disabled = true;
+    } else {
+      document.getElementById("category0").disabled = false;
+      document.getElementById("category1").disabled = false;
+      document.getElementById("category2").disabled = false;
+      document.getElementById("category3").disabled = false;
+      document.getElementById("category5").disabled = false;
+      document.getElementById("category6").disabled = false;
+      document.getElementById("category7").disabled = false;
+      document.getElementById("category8").disabled = false;
+      document.getElementById("category9").disabled = false;
+    }
+  }
+  function categoryflg5(checked){
+    if(checked == true){
+      document.getElementById("category0").disabled = true;
+      document.getElementById("category1").disabled = true;
+      document.getElementById("category2").disabled = true;
+      document.getElementById("category3").disabled = true;
+      document.getElementById("category4").disabled = true;
+      document.getElementById("category6").disabled = true;
+      document.getElementById("category7").disabled = true;
+      document.getElementById("category8").disabled = true;
+      document.getElementById("category9").disabled = true;
+    } else {
+      document.getElementById("category0").disabled = false;
+      document.getElementById("category1").disabled = false;
+      document.getElementById("category2").disabled = false;
+      document.getElementById("category3").disabled = false;
+      document.getElementById("category4").disabled = false;
+      document.getElementById("category6").disabled = false;
+      document.getElementById("category7").disabled = false;
+      document.getElementById("category8").disabled = false;
+      document.getElementById("category9").disabled = false;
+    }
+  }
+  function categoryflg6(checked){
+    if(checked == true){
+      document.getElementById("category0").disabled = true;
+      document.getElementById("category1").disabled = true;
+      document.getElementById("category2").disabled = true;
+      document.getElementById("category3").disabled = true;
+      document.getElementById("category4").disabled = true;
+      document.getElementById("category5").disabled = true;
+      document.getElementById("category7").disabled = true;
+      document.getElementById("category8").disabled = true;
+      document.getElementById("category9").disabled = true;
+    } else {
+      document.getElementById("category0").disabled = false;
+      document.getElementById("category1").disabled = false;
+      document.getElementById("category2").disabled = false;
+      document.getElementById("category3").disabled = false;
+      document.getElementById("category4").disabled = false;
+      document.getElementById("category5").disabled = false;
+      document.getElementById("category7").disabled = false;
+      document.getElementById("category8").disabled = false;
+      document.getElementById("category9").disabled = false;
+    }
+  }
+  function categoryflg7(checked){
+    if(checked == true){
+      document.getElementById("category0").disabled = true;
+      document.getElementById("category1").disabled = true;
+      document.getElementById("category2").disabled = true;
+      document.getElementById("category3").disabled = true;
+      document.getElementById("category4").disabled = true;
+      document.getElementById("category5").disabled = true;
+      document.getElementById("category6").disabled = true;
+      document.getElementById("category8").disabled = true;
+      document.getElementById("category9").disabled = true;
+    } else {
+      document.getElementById("category0").disabled = false;
+      document.getElementById("category1").disabled = false;
+      document.getElementById("category2").disabled = false;
+      document.getElementById("category3").disabled = false;
+      document.getElementById("category4").disabled = false;
+      document.getElementById("category5").disabled = false;
+      document.getElementById("category6").disabled = false;
+      document.getElementById("category8").disabled = false;
+      document.getElementById("category9").disabled = false;
+    }
+  }
+  function categoryflg8(checked){
+    if(checked == true){
+      document.getElementById("category0").disabled = true;
+      document.getElementById("category1").disabled = true;
+      document.getElementById("category2").disabled = true;
+      document.getElementById("category3").disabled = true;
+      document.getElementById("category4").disabled = true;
+      document.getElementById("category5").disabled = true;
+      document.getElementById("category6").disabled = true;
+      document.getElementById("category7").disabled = true;
+      document.getElementById("category9").disabled = true;
+    } else {
+      document.getElementById("category0").disabled = false;
+      document.getElementById("category1").disabled = false;
+      document.getElementById("category2").disabled = false;
+      document.getElementById("category3").disabled = false;
+      document.getElementById("category4").disabled = false;
+      document.getElementById("category5").disabled = false;
+      document.getElementById("category6").disabled = false;
+      document.getElementById("category7").disabled = false;
+      document.getElementById("category9").disabled = false;
+    }
+  }
+  function categoryflg9(checked){
+    if(checked == true){
+      document.getElementById("category0").disabled = true;
+      document.getElementById("category1").disabled = true;
+      document.getElementById("category2").disabled = true;
+      document.getElementById("category3").disabled = true;
+      document.getElementById("category4").disabled = true;
+      document.getElementById("category5").disabled = true;
+      document.getElementById("category6").disabled = true;
+      document.getElementById("category7").disabled = true;
+      document.getElementById("category8").disabled = true;
+    } else {
+      document.getElementById("category0").disabled = false;
+      document.getElementById("category1").disabled = false;
+      document.getElementById("category2").disabled = false;
+      document.getElementById("category3").disabled = false;
+      document.getElementById("category4").disabled = false;
+      document.getElementById("category5").disabled = false;
+      document.getElementById("category6").disabled = false;
+      document.getElementById("category7").disabled = false;
+      document.getElementById("category8").disabled = false;
+    }
+  }
