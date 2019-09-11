c46efae890a8c33ebdbc7a4eddccaac70691fd75
DaisukeTakuma <daisuke.takuma123@gmail.com>
2019-09-11 17:43:18 +0900

    微修正

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -60,7 +60,7 @@ class PostsController < ApplicationController
     #:category_nameは、カテゴリ名と0(チェックなし)または1(チェックあり)がペアの2次元配列
     params[:category_name].each do |cn1,cn2|
       #チェックが入っていたカテゴリ名をDBに登録
-      if title.blank? || description.blank? || cn2 == "1"
+      if !title.blank? || !description.blank? || cn2 == "1"
         post = current_user.posts.new(post_params)
         post.category_name = cn1
         #if文で保存すると、複数回render、redirectしてしまうため使用不可
